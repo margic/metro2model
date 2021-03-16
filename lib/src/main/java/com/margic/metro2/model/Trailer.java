f443952f696c8d14943ae2c1077516d1a8f3bb1a
Paul <pmcrofts@margic.com>
2021-03-16 12:39:14 -0700

    Added trailer and pom gradle build changes

---
+++ lib/src/main/java/com/margic/metro2/model/Trailer.java
@@ -0,0 +1,53 @@
+package com.margic.metro2.model;
+
+import lombok.Data;
+
+@Data
+public class Trailer{
+    private int recordWordDescriptor;
+    private String recordIdentifier;
+    private int totalBaseRecords;
+    private String reserved;
+    private int totalStatusCodeDF;
+    private int totalJ1Segments;
+    private int totalJ2Segments;
+    private int blockCount;
+    private int totalStatusCodeDA;
+    private int totalStatusCode05;
+    private int totalStatusCode11;
+    private int totalStatusCode13;
+    private int totalStatusCode61;
+    private int totalStatusCode62;
+    private int totalStatusCode63;
+    private int totalStatusCode64;
+    private int totalStatusCode65;
+    private int totalStatusCode71;
+    private int totalStatusCode78;
+    private int totalStatusCode80;
+    private int totalStatusCode82;
+    private int totalStatusCode83;
+    private int totalStatusCode84;
+    private int totalStatusCode88;
+    private int totalStatusCode89;
+    private int totalStatusCode93;
+    private int totalStatusCode94;
+    private int totalStatusCode95;
+    private int totalStatusCode96;
+    private int totalStatusCode97;
+    private int totalECOACodeZ;
+    private int totalEmploymentSegements;
+    private int totalOriginalCreditorSegments;
+    private int totalPurchasedSegments;
+    private int totalPaymentInformationSegments;
+    private int totalChangeSegments;
+    private int totalSocialNumbersAllSegments;
+    private int totalSocialNumbersBaseSegments;
+    private int totalSocialNumbersJ1Segments;
+    private int totalSocialNumbersJ2Segments;
+    private int totalDatesBirthAllSegments;
+    private int totalDatesBirthBaseSegments;
+    private int totalDatesBirthJ1Segments;
+    private int totalDatesBirthJ2Segments;
+    private int totalTelephoneNumbersAllSegments;
+
+}
